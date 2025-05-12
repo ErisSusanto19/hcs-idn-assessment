71c95eff57dd43d4ea9129958d98fb75eb642072
Eris Susanto <erissusanto997@gmail.com>
2025-05-12 11:06:14 +0700

    beginning setup application properties and environtment

---
+++ src/main/java/com/hcs_idn/api_assessment/ApiAssessmentApplication.java
@@ -0,0 +1,18 @@
+package com.hcs_idn.api_assessment;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAssessmentApplication {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+		SpringApplication.run(ApiAssessmentApplication.class, args);
+	}
+
+}
